4eedc986466757d5b7663444c883f6e6f899a3ae
Max Romanov <max.romanov@gmail.com>
2020-03-06 16:04:28 +0300

    unit-python 1.15.0 (new formula)

---
+++ Formula/unit-python.rb
@@ -0,0 +1,72 @@
+class UnitPython < Formula
+  desc "Python module for Unit application server"
+  homepage "https://unit.nginx.org"
+  url "https://unit.nginx.org/download/unit-1.15.0.tar.gz"
+  sha256 "f8b231d02865b5695825a2a77ecbfd0cff33c0954c0d351d2d773d7e5f6237dd"
+  head "https://hg.nginx.org/unit", :using => :hg
+
+  depends_on "openssl@1.1"
+  depends_on "unit@1.15.0"
+  uses_from_macos "python"
+
+  def install
+    system "./configure",
+              "--prefix=#{prefix}",
+              "--sbindir=#{bin}",
+              "--log=#{var}/log/unit/unit.log",
+              "--pid=#{var}/run/unit/unit.pid",
+              "--control=unix:#{var}/run/unit/control.sock",
+              "--modules=#{HOMEBREW_PREFIX}/lib/unit/modules",
+              "--state=#{var}/state/unit",
+              "--openssl",
+              "--cc-opt=-I#{Formula["openssl@1.1"].opt_prefix}/include",
+              "--ld-opt=-L#{Formula["openssl@1.1"].opt_prefix}/lib"
+
+    inreplace "build/autoconf.data",
+        "NXT_MODULES='#{HOMEBREW_PREFIX}/lib/unit/modules'",
+        "NXT_MODULES='#{lib}/unit/modules'"
+
+    system "./configure", "python"
+    system "make", "python-install"
+  end
+
+  test do
+    require "socket"
+
+    server = TCPServer.new(0)
+    port = server.addr[1]
+    server.close
+
+    expected_output = "Hello world!"
+    (testpath/"unit.conf").write <<~EOS
+      {
+        "listeners": { "*:#{port}": { "pass": "applications/test" } },
+        "applications": {
+          "test": { "type": "python 2", "path": "#{testpath}", "module": "wsgi" }
+        }
+      }
+    EOS
+    (testpath/"wsgi.py").write <<~EOS
+      def application(environ, start_response):
+          start_response('200 OK', [('Content-type', 'text/plain')])
+          return b"#{expected_output}"
+    EOS
+    (testpath/"state/certs").mkpath
+
+    system "#{HOMEBREW_PREFIX}/bin/unitd", "--log", "#{testpath}/unit.log",
+                        "--control", "unix:#{testpath}/control.sock",
+                        "--pid", "#{testpath}/unit.pid",
+                        "--state", "#{testpath}/state"
+    sleep 3
+
+    pid = File.open(testpath/"unit.pid").gets.chop.to_i
+
+    system "curl", "-s", "--unix-socket", "#{testpath}/control.sock",
+                    "-X", "PUT",
+                    "-d", "@#{testpath}/unit.conf", "127.0.0.1/config"
+
+    assert_match expected_output, shell_output("curl -s 127.0.0.1:#{port}")
+  ensure
+    Process.kill("TERM", pid)
+  end
+end

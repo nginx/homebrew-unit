d684976c930c73ff90d416785a97fef7e06888a3
Max Romanov <max.romanov@gmail.com>
2021-12-02 21:27:37 +0300

    php-embed@7.4 7.4.26

--- Formula/php-embed@7.4.rb
+++ Formula/php-embed@7.4.rb
@@ -2,9 +2,9 @@ class PhpEmbedAT74 < Formula
   desc "PHP library for embedding in applications"
   homepage "https://www.php.net/"
   # Should only be updated if the new version is announced on the homepage, https://www.php.net/
-  url "https://www.php.net/distributions/php-7.4.25.tar.xz"
-  mirror "https://fossies.org/linux/www/php-7.4.25.tar.xz"
-  sha256 "12a758f1d7fee544387a28d3cf73226f47e3a52fb3049f07fcc37d156d393c0a"
+  url "https://www.php.net/distributions/php-7.4.26.tar.xz"
+  mirror "https://fossies.org/linux/www/php-7.4.26.tar.xz"
+  sha256 "e305b3aafdc85fa73a81c53d3ce30578bc94d1633ec376add193a1e85e0f0ef8"
 
   depends_on "pkg-config" => :build
   depends_on "php@7.4"
@@ -104,14 +104,14 @@ class PhpEmbedAT74 < Formula
       --enable-embed=shared
       --with-bz2#{headers_path}
       --with-curl
+      --with-external-gd
+      --with-external-pcre
       --with-ffi
       --with-fpm-user=_www
       --with-fpm-group=_www
-      --with-freetype
       --with-gettext=#{Formula["gettext"].opt_prefix}
       --with-gmp=#{Formula["gmp"].opt_prefix}
       --with-iconv#{headers_path}
-      --with-jpeg
       --with-kerberos
       --with-layout=GNU
       --with-ldap=#{Formula["openldap"].opt_prefix}
@@ -135,7 +135,6 @@ class PhpEmbedAT74 < Formula
       --with-sqlite3
       --with-tidy=#{Formula["tidy-html5"].opt_prefix}
       --with-unixODBC
-      --with-webp
       --with-xmlrpc
       --with-xsl
       --with-zip

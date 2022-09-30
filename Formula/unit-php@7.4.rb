258267ae403ad0c21f7e1611ef37f0c58fd03f9b
Konstantin Pavlov <thresh@videolan.org>
2022-09-30 17:04:04 +0400

    Added unit-php@7.4 formula.

---
+++ Formula/unit-php@7.4.rb
@@ -0,0 +1,71 @@
+class UnitPhpAT74 < Formula
+  desc "PHP module for Unit application server"
+  homepage "https://unit.nginx.org"
+  url "https://unit.nginx.org/download/unit-1.28.0.tar.gz"
+  sha256 "69779208ed2d1687b5d26f8c4e0f8460444eb285fb6ced2443652e0c584a80b0"
+  head "https://hg.nginx.org/unit", using: :hg
+
+  depends_on "openssl@1.1"
+  depends_on "php-embed@7.4"
+  depends_on "unit@1.28.0"
+
+  def install
+    system "./configure",
+              "--prefix=#{prefix}",
+              "--sbindir=#{bin}",
+              "--log=#{var}/log/unit/unit.log",
+              "--pid=#{var}/run/unit/unit.pid",
+              "--control=unix:#{var}/run/unit/control.sock",
+              "--modules=#{HOMEBREW_PREFIX}/lib/unit/modules",
+              "--state=#{var}/state/unit",
+              "--tmp=/tmp",
+              "--openssl",
+              "--cc-opt=-I#{Formula["openssl@1.1"].opt_prefix}/include",
+              "--ld-opt=-L#{Formula["openssl@1.1"].opt_prefix}/lib"
+
+    inreplace "build/autoconf.data",
+        "NXT_MODULES='#{HOMEBREW_PREFIX}/lib/unit/modules'",
+        "NXT_MODULES='#{lib}/unit/modules'"
+
+    system "./configure", "php", "--module=php74"
+    system "make", "php74-install"
+  end
+
+  test do
+    require "socket"
+
+    server = TCPServer.new(0)
+    port = server.addr[1]
+    server.close
+
+    expected_output = "Hello world!"
+    (testpath/"unit.conf").write <<~EOS
+      {
+        "listeners": { "*:#{port}": { "pass": "applications/test" } },
+        "applications": {
+          "test": { "type": "php", "root": "#{testpath}" }
+        }
+      }
+    EOS
+    (testpath/"index.php").write <<~EOS
+      <?php print("#{expected_output}"); ?>
+    EOS
+    (testpath/"state/certs").mkpath
+
+    system "#{HOMEBREW_PREFIX}/bin/unitd", "--log", "#{testpath}/unit.log",
+                        "--control", "unix:#{testpath}/control.sock",
+                        "--pid", "#{testpath}/unit.pid",
+                        "--state", "#{testpath}/state"
+    sleep 3
+
+    pid = File.open(testpath/"unit.pid").gets.chop.to_i
+
+    system "curl", "-s", "--unix-socket", "#{testpath}/control.sock",
+                    "-X", "PUT",
+                    "-d", "@#{testpath}/unit.conf", "127.0.0.1/config"
+
+    assert_match expected_output, shell_output("curl -s 127.0.0.1:#{port}")
+  ensure
+    Process.kill("TERM", pid)
+  end
+end

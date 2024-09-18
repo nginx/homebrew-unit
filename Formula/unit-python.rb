3fd06d0132c753e42dc7357c8dc764937c48e909
Konstantin Pavlov <thresh@nginx.com>
2024-09-17 22:37:51 -0700

    Use modulesdir configure argument

--- Formula/unit-python.rb
+++ Formula/unit-python.rb
@@ -21,7 +21,7 @@ class UnitPython < Formula
               "--runstatedir=#{var}/run",
               "--pid=#{var}/run/unit/unit.pid",
               "--control=unix:#{var}/run/unit/control.sock",
-              "--modules=#{HOMEBREW_PREFIX}/lib/unit/modules",
+              "--modulesdir=#{HOMEBREW_PREFIX}/lib/unit/modules",
               "--statedir=#{var}/state/unit",
               "--tmpdir=/tmp",
               "--openssl",

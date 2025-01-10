4bb648fc6987b2813bfe8598a7a8866f809c8c50
Konstantin Pavlov <thresh@nginx.com>
2025-01-10 14:49:54 -0800

    php-embed: fix link warnings on Ubuntu

--- Formula/php-embed.rb
+++ Formula/php-embed.rb
@@ -41,6 +41,14 @@ class PhpEmbed < Formula
   depends_on "tidy-html5"
   depends_on "unixodbc"
 
+  uses_from_macos "xz" => :build
+  uses_from_macos "bzip2"
+  uses_from_macos "libedit"
+  uses_from_macos "libffi", since: :catalina
+  uses_from_macos "libxml2"
+  uses_from_macos "libxslt"
+  uses_from_macos "zlib"
+
   on_macos do
     # PHP build system incorrectly links system libraries
     # see https://github.com/php/php-src/issues/10680
